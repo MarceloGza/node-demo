e4de3ec77ca8c4e0f58152193fb5ae3dcbbc1b38
Marcelo Garza <andresmarcelo99@hotmail.com>
2021-02-13 00:05:44 -0600

    changed templating engine to EJS
    deleted HTML and PUG files

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(express.urlencoded({ extended: true }));
@@ -15,7 +15,7 @@ app.use('/admin', adminData.router);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { docTitle: '404' });
+  res.status(404).render('404', { docTitle: '404', ref: '' });
 });
 
 app.listen(3000);
