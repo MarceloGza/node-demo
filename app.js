04b430cc3ca4b3d7c97477532264ba15793b7b65
Marcelo Garza <andresmarcelo99@hotmail.com>
2021-02-13 22:19:08 -0600

    added Controllers to follow MVC

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error')
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -11,11 +12,9 @@ app.set('views', 'views');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.router);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { docTitle: '404', ref: '' });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,24 +1,12 @@
 const express = require('express');
 
-const path = require('path');
-
-const rootDir = require('../util/rootDir');
+const productsController = require('../controllers/products')
 
 const router = express.Router();
 
-const products = [];
+router.post('/add-product', productsController.postAddProduct);
 
-router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect('/');
-});
+router.get('/add-product', productsController.getAddProduct);
 
-router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {
-    docTitle: 'Add Product',
-    ref: '/admin/add-product',
-  });
-});
+module.exports = router;
 
-module.exports.router = router;
-module.exports.products = products;
